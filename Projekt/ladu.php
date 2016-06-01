f8c468a3fd5d46990ec00a507947646ea2335007
Kristi Spelman <kristi.spelman@just.sise>
2016-06-01 17:35:02 +0300

    lisatud vaatefailid

---
+++ Projekt/ladu.php
@@ -0,0 +1,40 @@
+<?php
+require_once('funktsioonid.php');
+session_start();
+connect_db();
+
+$page="avaleht";
+if (isset($_GET['page']) && $_GET['page']!=""){
+	$page=htmlspecialchars($_GET['page']);
+}
+
+include_once('views/head.html');
+
+switch($page){
+	case "login":
+		logi();
+	break;
+	case "tooted":
+		kuva_tooted();
+	break;
+	case "logout":
+		logout();
+	break;
+	case "lisa":
+		lisa();
+	break;
+	case "kustutamine":
+		kustuta();
+	break;
+	case "muutmine":
+		muuda();
+	break;
+
+	default:
+		include_once('views/avaleht.html');
+	break;
+}
+
+include_once('views/foot.html');
+
+?>
